ee0181f3860d50b9463bb75bb71184c909ead2a4
alexfleury <28400108+alexfleury@users.noreply.github.com>
2024-11-09 11:39:58 -0500

    Removed kitty notifications.

--- user/kitty.nix
+++ user/kitty.nix
@@ -9,7 +9,7 @@
     settings = {
       background_opacity = 0.9;
       enable_audio_bell = "no";
-      notify_on_cmd_finish = "unfocused";
+      notify_on_cmd_finish = "never";
       confirm_os_window_close = 0;
     };
   };
