3cc8bf38083910d5233102fd1474651f92d018c0
alexfleury <28400108+alexfleury@users.noreply.github.com>
2024-12-06 18:07:35 -0500

    Changed wallpaper to black.

--- user/desktop/hyprland/hyprpaper.nix
+++ user/desktop/hyprland/hyprpaper.nix
@@ -1,6 +1,7 @@
 { ... }:
 let
-  wallpaperPath = ../../../wallpapers/PXL_20231125_173902958.jpg;
+  #wallpaperPath = ../../../wallpapers/PXL_20231125_173902958.jpg;
+  wallpaperPath = ../../../wallpapers/black.jpg;
 in
 {
   services.hyprpaper = {
