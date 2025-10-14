58e12e3c68ccf88bd2b2b91f46d4eea0a45b66fb
alexfleury <28400108+alexfleury@users.noreply.github.com>
2025-10-13 22:53:46 -0400

    Future proofing ssh options.

--- user/cli/ssh.nix
+++ user/cli/ssh.nix
@@ -3,12 +3,29 @@
 {
   programs.ssh = {
     enable = true;
-    enableDefaultConfig = true;
+    # Will be removed in the future.
+    enableDefaultConfig = false;
 
-    matchBlocks."pihole" = {
-      hostname = "192.168.0.14";
-      user = "alexandre";
-      port= 22;
+    matchBlocks = {
+      # Default config.
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+
+      "pihole" = {
+        hostname = "192.168.0.14";
+        user = "alexandre";
+        port= 22;
+      };
     };
   };
 }
